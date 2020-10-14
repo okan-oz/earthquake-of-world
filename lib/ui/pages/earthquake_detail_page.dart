d6e3839d9acd304ea721eefaba796d53a505789e
okan-oz <62757704+okan-oz@users.noreply.github.com>
2020-10-15 00:16:40 +0300

    updated --ooz

--- lib/ui/pages/earthquake_detail_page.dart
+++ lib/ui/pages/earthquake_detail_page.dart
@@ -1,5 +1,8 @@
+import 'package:earthquakes_of_world/common/utils/utils.dart';
 import 'package:earthquakes_of_world/models/models.dart';
+import 'package:earthquakes_of_world/ui/widgets/map_samle.dart';
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class EartquakeDetailPage extends StatefulWidget {
   EartquakeDetailPage(this.earthquake);
@@ -9,8 +12,142 @@ class EartquakeDetailPage extends StatefulWidget {
 }
 
 class _EartquakeDetailPageState extends State<EartquakeDetailPage> {
+ TextStyle _getAppBarDetailTextStyle() {
+    return TextStyle(color: Colors.white, fontWeight: FontWeight.bold);
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Container(
+      child: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: <Widget>[
+          SliverAppBar(
+            stretch: true,
+            onStretchTrigger: () {
+              return;
+            },
+            expandedHeight: 130.0,
+            flexibleSpace: FlexibleSpaceBar(
+              stretchModes: <StretchMode>[
+                StretchMode.zoomBackground,
+                StretchMode.blurBackground,
+                StretchMode.fadeTitle,
+              ],
+              centerTitle: false,
+              titlePadding:
+                  EdgeInsetsDirectional.only(start: 10, bottom: 16, top: 10),
+              title: ListTile(
+                leading: CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Utils.DecideListTileColor(widget.earthquake.properties.mag),
+                  child: Text(widget.earthquake.properties.mag.toString(),
+                      style: _getAppBarDetailTextStyle()),
+                ),
+                title: Text(
+                  widget.earthquake.properties.title,
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  widget.earthquake.properties.time.toString(),
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.asset(
+                    "assets/depremAppBar.jpg",
+                    fit: BoxFit.cover,
+                    colorBlendMode: BlendMode.modulate,
+                    color: Color.fromRGBO(255, 255, 255, 0.9),
+                  ),
+                  const DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment(0.0, 0.5),
+                        end: Alignment(0.0, 0.0),
+                        colors: <Color>[
+                          Color(0x60000000),
+                          Color(0x00000000),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              ListTile(
+                leading: Icon(
+                  Icons.dialpad,
+                  color: Colors.brown,
+                ),
+                title: Text("Derinlik"),
+                subtitle: Text(widget.earthquake.properties.dmin.toString()),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.timer,
+                  color: Colors.green,
+                ),
+                title: Text("Tarih"),
+                subtitle: Text(widget.earthquake.properties.time.toString()),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.location_on,
+                  color: Colors.red,
+                ),
+                title: Text("Lokasyon"),
+                subtitle: Text(widget.earthquake.properties.mag.toString()),
+              ),
+              ListTile(
+                leading: IconButton(
+                  icon: Icon(
+                    Icons.share,
+                    color: Colors.blue,
+                    semanticLabel: "Paylaş",
+ 
+                  ),
+                  onPressed: () {
+                    final RenderBox box = context.findRenderObject();
+                    Share.share(
+                        "${widget.earthquake.properties.time.toString()} tarihinde , ${widget.earthquake.properties.mag} lokasyonunda , şiddeti ${widget.earthquake.properties.mag.toString()} olan deprem oldu . ",
+                        subject: "Deprem oldu !",
+                        sharePositionOrigin:
+                            box.localToGlobal(Offset.zero) & box.size);
+                  },
+                ),
+                title: Text("Paylaş"),
+                subtitle: Text("Sosyal Medyada Paylaş"),
+              ),
+              // ListTiles++
+            ]),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              height: 350,
+              width: 50,
+              child: Container()//MapSample(widget.earthquake),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
+
+
+  
---
+++ lib/ui/widgets/map_samle.dart
@@ -0,0 +1,108 @@
+import 'dart:async';
+import 'package:earthquakes_of_world/models/earthquakes.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+class MapSample extends StatefulWidget {
+  Feature eq;
+
+  MapSample(this.eq);
+
+  @override
+  State<MapSample> createState() => MapSampleState();
+
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+  final Set<Marker> _markers = Set();
+  double _zoom = 7;
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+
+  LatLng _lastMapPosition = _center;
+  CameraPosition _initialPosition =
+      CameraPosition(target: LatLng(38.4315, 39.1505));
+MapType _defaultMapType = MapType.hybrid;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        GoogleMap(
+          markers: _markers,
+          mapType: _defaultMapType,
+          initialCameraPosition: _initialPosition,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+          onCameraMove: (position) {
+            _lastMapPosition = position.target;
+          },
+        ),
+         Container(
+              margin: EdgeInsets.only(top: 80, right: 10),
+              alignment: Alignment.topRight,
+              child: Column(
+                children: <Widget>[
+                  FloatingActionButton(
+                      child: Icon(Icons.layers),
+                      elevation: 5,
+                      backgroundColor: Colors.teal[200],
+                      onPressed: () {
+                       _changeMapType();
+                        print('Changing the Map Type');
+                      }),
+                ]),
+            )
+      ],
+    );
+  }
+
+void _changeMapType() {
+    setState(() {
+      _defaultMapType = _defaultMapType == MapType.normal ? MapType.hybrid : MapType.normal;
+    });
+  }
+
+
+
+  Future<void> _goToEarthQuake() async {
+    double lat = widget.eq.geometry.coordinates.first;
+    double long = widget.eq.geometry.coordinates.last;
+  
+
+//BitmapDescriptor customIcon;
+
+ 
+
+
+    GoogleMapController controller = await _controller.future;
+    controller
+        .animateCamera(CameraUpdate.newLatLngZoom(LatLng(lat, long), _zoom));
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId(widget.eq.properties.place),
+          position: LatLng(lat, long),
+          infoWindow: InfoWindow(title: widget.eq.properties.mag.toString(),snippet: widget.eq.properties.place),
+          //icon: BitmapDescriptor.fromAsset("assets/depremAppBar.jpg",),
+        
+      
+        ),
+      );
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _goToEarthQuake();
+  }
+
+@override
+  void dispose() {
+    super.dispose();
+  }
+
+}
