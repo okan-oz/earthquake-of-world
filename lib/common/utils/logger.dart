8df72919ff7a7a2ae225afef152b5e4df55b90d5
okan-oz <62757704+okan-oz@users.noreply.github.com>
2020-10-24 00:00:10 +0300

    updated --ooz

---
+++ lib/common/utils/logger.dart
@@ -0,0 +1,24 @@
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+// import 'package:flutter/material.dart';
+
+class Logger {
+  static Future<void> initializeFlutterFire() async {
+    // // Wait for Firebase to initialize
+    // await Firebase.initializeApp();
+
+    // await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+
+    // // Pass all uncaught errors to Crashlytics.
+    // Function originalOnError = FlutterError.onError;
+    // FlutterError.onError = (FlutterErrorDetails errorDetails) async {
+    //   await FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+
+    //   originalOnError(errorDetails);
+    // };
+  }
+
+ static Future<void> recordError(dynamic e, StackTrace s, String reason) async {
+    // await FirebaseCrashlytics.instance.recordError(e, s, reason: reason);
+  }
+}
--- lib/ui/pages/earthquake_list_page.dart
+++ lib/ui/pages/earthquake_list_page.dart
@@ -1,20 +1,30 @@
+import 'package:earthquakes_of_world/common/utils/logger.dart';
 import 'package:earthquakes_of_world/models/models.dart';
 import 'package:earthquakes_of_world/provider/earthquake_filter_provider.dart';
 import 'package:earthquakes_of_world/ui/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 class EarthquakeListPage extends StatelessWidget {
   EarthquakeListPage(this.eqFilter);
   final EarthquakeFilter eqFilter;
   @override
   Widget build(BuildContext context) {
+    try {
+      throw 'error_example';
+    } catch (e, s) {
+      Logger.recordError(e, s, 'Error example');
+    }
+
     return Scaffold(
       appBar: CustomSearchAppBar(),
-      body:RefreshIndicator(child:  EarthquakeList(eqFilter),
-      onRefresh: ()async{
-         context.read<EarthquakeFilterProvider>().refresh();
-         return await Future.delayed(Duration(seconds: 1));
-      },),
+      body: RefreshIndicator(
+        child: EarthquakeList(eqFilter),
+        onRefresh: () async {
+          context.read<EarthquakeFilterProvider>().refresh();
+          return await Future.delayed(Duration(seconds: 1));
+        },
+      ),
     );
   }
 }
